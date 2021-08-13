035f5ea57ba7600ed8e607319f18e44f110b91e0
DESKTOP-0KACA77\hp <priteshbhangale04@gmail.com>
2021-08-13 16:39:41 +0530

    Calculated Employee wage per Month by creating method

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -1,57 +1,65 @@
 package com.employeewage;
 
 public class EmployeeWage {
-	public static void main(String[] args) {
-		int max=1;
-		int min=0;
-		int flag=0;
-		int SalHour=20, fullday=8;
-		int partTime=8;
+	public int SalHour = 20, fullday = 8;
+	public void employeeWage(int flag) {
 		
-		int  a = (int) (Math.random()*(max-min+1)+min);   
-		System.out.println("Random no is " + a);
-		if(a==1)
-		{
-			flag=1;
+		
+		if (flag == 1) {
+			int WageperMonth = fullday * 20 * SalHour;
+			System.out.println("WageperMonth is " + WageperMonth);
 		}
-		if(flag==1)
-		{
+		else {
+			System.out.println("Employee is absent");
+		}
+	}
+
+	public static void main(String[] args) {
+		int max = 1;
+		int min = 0;
+		int flag = 0;
+		int SalHour = 20, fullday = 8;
+		int partTime = 8;
+		EmployeeWage employeeWage= new EmployeeWage();
+		int a = (int) (Math.random() * (max - min + 1) + min);
+		System.out.println("Random no is " + a);
+		if (a == 1) {
+			flag = 1;
+		}
+		if (flag == 1) {
 			System.out.println("Employee Present");
 		}
-		if(flag==0)
-		{
+		if (flag == 0) {
 			System.out.println("Employee Absent");
 		}
-		
-		if(flag==1)
-		{
-		int DailyWage= fullday*SalHour;
-		System.out.println("DailyWage is "+ DailyWage);
+
+		if (flag == 1) {
+			int DailyWage = fullday * SalHour;
+			System.out.println("DailyWage is " + DailyWage);
 		}
-			
-		int mn=0,mx=1;
-		int  choice = (int) (Math.random()*(mx-mn+1)+mn);
+
+		int mn = 0, mx = 1;
+		int choice = (int) (Math.random() * (mx - mn + 1) + mn);
 		switch (choice) {
-		case 0 : int fullTime=8;
-		System.out.println("Fulltime wage is "+ fullTime*SalHour);
-		break;
-		case 1:int HalfTime=4;
-		System.out.println("Halftime wage is "+ HalfTime*SalHour);
-		break;
+		case 0:
+			int fullTime = 8;
+			System.out.println("Fulltime wage is " + fullTime * SalHour);
+			break;
+		case 1:
+			int HalfTime = 4;
+			System.out.println("Halftime wage is " + HalfTime * SalHour);
+			break;
 
-		default : break;
+		default:
+			break;
 		}
-		//UC5
-		if(flag==1) {
-		int WageperMonth= fullday*20 * SalHour;
-		System.out.println("WageperMonth is " + WageperMonth);
+		// UC5
+		if (flag == 1) {
+			int WageperMonth = fullday * 20 * SalHour;
+			System.out.println("WageperMonth is " + WageperMonth);
 		}
+		employeeWage.employeeWage(flag);
 
-		
-		
-		
-		
-		
 	}
 
 }
